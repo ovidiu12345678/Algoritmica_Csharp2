c7d5d0680f19a1dfb5fae22ed0026c3f0ca78de8
OVY <OVY@DESKTOP-Q2TVGH5>
2022-11-01 11:15:57 +0200

    Add project files.

---
+++ Algoritmica_Csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Aplicatie_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PROGRAMUL CONVERTESTE ORICE NUMAR IN FORMAT DOUBLE IN INT
+
+
+            Console.WriteLine("Introduceti valorile corespunzatoare pentru conversia inplicita !");
+
+            Console.WriteLine("Introduceti prima valoare !");
+            int theFristNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce-ti ultima valoare corespunzatoare !");
+            double theLastNumber = double.Parse(Console.ReadLine());
+
+            if (theFristNumber <= 0 && theLastNumber <= 0 || theLastNumber <= 0 && theLastNumber <= 0 || theFristNumber <= 0 && theFristNumber != theLastNumber || theLastNumber <= 0 && theLastNumber != theFristNumber && theFristNumber <= 0 && theFristNumber != theLastNumber)
+            {
+                Console.WriteLine("Valorile ce lea-ti introdus nu sunt acceptate, introduce-ti doar valori pozitive !");
+                return;
+            }
+            else
+            {
+                theLastNumber = theFristNumber;
+
+                Console.WriteLine(theLastNumber);
+            }
+        }
+    }
+}
